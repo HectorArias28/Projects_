3bbf71dc40abb246b8d49c8ad50e42218cab33d8
Hector <hectorarias8716@gmail.com>
2018-12-07 17:34:12 -0500

    W2D5/My_Hash_Map/lib/p03_hash_set.rb

--- Daytime_projects/W2D5/My_Hash_Map/lib/p03_hash_set.rb
+++ Daytime_projects/W2D5/My_Hash_Map/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -10,15 +10,17 @@ class HashSet
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
