57f3d5a8e71c3a85f46267dfd68dbcf44a3e1b7a
Hector <hectorarias8716@gmail.com>
2018-12-07 16:50:43 -0500

    W2D5/My_Hash_Map/lib/p01_int_set.rb

--- Daytime_projects/W2D5/My_Hash_Map/lib/p01_int_set.rb
+++ Daytime_projects/W2D5/My_Hash_Map/lib/p01_int_set.rb
@@ -65,7 +65,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -73,18 +73,24 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if self.include(num)
+    self[num] << num
+    self.count += 1
+    resize! if num_buckets < self.count
   end
 
   def remove(num)
+    self.count -= 1 if self[num].delete(num)
   end
 
   def include?(num)
+    self.store[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+   self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -92,5 +98,10 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) }
   end
 end
