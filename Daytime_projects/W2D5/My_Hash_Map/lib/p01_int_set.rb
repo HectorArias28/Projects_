2a2d45a8f236595c2dd93d2ce4c4d820201db725
Hector <hectorarias8716@gmail.com>
2018-12-07 15:45:54 -0500

    W2D5/My_Hash_Map/

--- Daytime_projects/W2D5/My_Hash_Map/lib/p01_int_set.rb
+++ Daytime_projects/W2D5/My_Hash_Map/lib/p01_int_set.rb
@@ -34,23 +34,29 @@ end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return false if self.include?(num)
+    self[num] << num
+    num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
